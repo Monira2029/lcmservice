525e27ea40ee69b5afc36974457f89df2ed6b63a
Monira2029 <msultana25799@gmail.com>
2025-10-06 01:13:04 +0600

    Fix: bind to Railway PORT

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
-
 var builder = WebApplication.CreateBuilder(args);
+
+// 🚨 THIS IS IMPORTANT for Railway deployment
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(int.Parse(port));
+});
+
 builder.Services.AddControllers();
+
 var app = builder.Build();
 app.MapControllers();
 app.Run();
